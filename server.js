1aab344bf2be8620145cceab4c4c4ca0809f7e76
CataReygadasG <catafranrey@gmail.com>
2023-11-15 18:09:36 -0300

    entrega3

--- server.js
+++
@@ -1,9 +0,0 @@
-//import express from "express";
-const http = require("http");
-const server = http.createServer((request, response)=>{
-    response.end("mi primer hola mundo");
-    console.log("consola")
-});
-server.listen("8080", ()=>{
-    console.log("Servidor activo");
-});
